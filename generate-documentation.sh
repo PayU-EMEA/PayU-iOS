8fd1bf135dadbb2cbde0a99cce6b9a7828324be0
Anton Ivashyna <anton.ivashyna@payu.com>
2023-03-24 13:00:26 +0100

    # 89. Updated docs.

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -1,13 +1,19 @@
 # https://developer.apple.com/documentation/xcode/distributing-documentation-to-external-developers
 
+XCODE_SCHEME="PUSDK"
+XCODE_DERIVED_DATA_PATH="docc"
+XCODE_DESTINATION="platform=iOS Simulator,OS=16.2,name=iPhone 14"
+XCODE_OTPUT_PATH="docs"
+
 # Removes `DerivedData` if exists
 rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -rf docc
 
 # Generates documentation for all packages
 xcodebuild docbuild \
-  -scheme PUSDK \
-  -derivedDataPath docc \
-  -destination 'platform=iOS Simulator,name=iPhone 14 Pro'
+  -scheme "$XCODE_SCHEME" \
+  -derivedDataPath "$XCODE_DERIVED_DATA_PATH" \
+  -destination "$XCODE_DESTINATION"
 
 # Wait till generated `PUSDK.doccarchive` being processed
 sleep 5
@@ -16,7 +22,7 @@ sleep 5
 cd docc/Build/Products/Debug-iphonesimulator
 $(xcrun --find docc) process-archive \
   transform-for-static-hosting PUSDK.doccarchive \
-  --output-path ../../../../docs \
+  --output-path ../../../../"$XCODE_OTPUT_PATH" \
   --hosting-base-path docc
 cd ../../../../
 rm -rf docc
